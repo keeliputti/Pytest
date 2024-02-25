64e7703980cce385dbaf43dd5d96315bf1f5ebf4
keeliputti <68512451+keeliputti@users.noreply.github.com>
2024-02-25 06:04:07 +0000

    adding mock function examples

---
+++ python_mocks/pro.py
@@ -0,0 +1,28 @@
+import requests
+
+def add(a, b):
+    return a + b
+
+def len_joke():
+    joke = get_joke()
+    return len(joke)
+
+def get_joke():
+    url = 'http://api.icndb.com/jokes/random'
+
+    # print(type(requests.exceptions.Timeout))
+
+    try:
+        response = requests.get(url)
+    except requests.exceptions.Timeout:
+        return 'No jokes'
+    except requests.exceptions.ConnectionError:
+        pass
+    if response.status_code == 200:
+        joke = response.json()['value']['joke']
+    else:
+        joke = 'No jokes'
+    return joke
+
+if __name__ == '__main__':
+    print(get_joke())
