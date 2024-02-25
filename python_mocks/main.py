6d695e64c13cc53ee98349b6d85c9c0c15fc0466
keeliputti <68512451+keeliputti@users.noreply.github.com>
2024-02-25 09:01:34 +0000

    adding side_effect e.g.

--- python_mocks/main.py
+++ python_mocks/main.py
@@ -9,6 +9,15 @@ def len_joke():
 
 def get_joke():
     url = 'http://api.icndb.com/jokes/random'
+    response = requests.get(url)
+    if response.status_code == 200:
+        joke = response.json()['value']['joke']
+    else:
+        joke = 'No jokes'
+    return joke
+
+def get_pro_joke():
+    url = 'http://api.icndb.com/jokes/random'
 
     # print(type(requests.exceptions.Timeout))
 
--- python_mocks/tests.py
+++ python_mocks/tests.py
@@ -4,9 +4,7 @@ from unittest.mock import MagicMock, patch
 import requests.exceptions
 from requests.exceptions import Timeout
 
-from pro import add, len_joke, get_joke
-# pro > basic
-# from basic import add, len_joke, get_joke
+from main import add, len_joke, get_joke, get_pro_joke
 
 class TestAdd(unittest.TestCase):
     def test_add(self):
@@ -14,12 +12,12 @@ class TestAdd(unittest.TestCase):
 
 class TestJoke(unittest.TestCase):
 
-    @patch('pro.get_joke')
+    @patch('main.get_joke')
     def test_len_joke(self, mock_get_joke):
         mock_get_joke.return_value = 'one'
         self.assertEqual(len_joke(), 3)
 
-    @patch('pro.requests')
+    @patch('main.requests')
     def test_get_joke(self, mock_requests):
         mock_response = MagicMock()
         mock_response.status_code = 200
@@ -30,7 +28,7 @@ class TestJoke(unittest.TestCase):
 
         self.assertEqual(get_joke(), 'hello world')
 
-    @patch('pro.requests')
+    @patch('main.requests')
     def test_fail_get_joke(self, mock_requests):
         mock_response = MagicMock(status_code=403)
         mock_response.json.return_value = {'value' : 
@@ -40,14 +38,14 @@ class TestJoke(unittest.TestCase):
 
         self.assertEqual(get_joke(), 'No jokes')
     
-    @patch('pro.requests')
+    @patch('main.requests')
     def test_get_joke_timeout_exception(self, mock_requests):
-        #fix: for bellow mentioned problem
+        #fix: for bellow mentioned mainblem
         mock_requests.exceptions = requests.exceptions
         mock_requests.get.side_effect = Timeout('Seems that the server is down')
         # TypeError: catching classes thst do not inherit frim the BaseException is not allowed.
         # MagicMock class/obj is not subclass of BaseException class
-        self.assertEqual(get_joke(), 'No jokes')
+        self.assertEqual(get_pro_joke(), 'No jokes')
 
 if __name__ == '__main__':
     unittest.main()
