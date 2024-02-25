224c5a3f2c43083e13bcafd3a2c945b91f003faa
keeliputti <68512451+keeliputti@users.noreply.github.com>
2024-02-25 15:52:14 +0000

    adding requirements.txt file

--- python_mocks/main.py
+++ python_mocks/main.py
@@ -16,21 +16,73 @@ def get_joke():
         joke = 'No jokes'
     return joke
 
-def get_pro_joke():
+def pro_get_joke():
     url = 'http://api.icndb.com/jokes/random'
 
     # print(type(requests.exceptions.Timeout))
 
     try:
-        response = requests.get(url)
+        response = requests.get(url, timeout=30)
     except requests.exceptions.Timeout:
-        return 'No jokes'
+        return 'Timeout Error'
     except requests.exceptions.ConnectionError:
         pass
     if response.status_code == 200:
         joke = response.json()['value']['joke']
     else:
         joke = 'No jokes'
+    return joke
+
+def advance_get_joke():
+    url = 'http://api.icndb.com/jokes/random'
+
+    # print(type(requests.exceptions.Timeout))
+
+    try:
+        response = requests.get(url, timeout=30)
+        response.raise_for_status()
+
+    except requests.exceptions.Timeout:
+        return 'Timeout Error'
+    
+    except requests.exceptions.ConnectionError:
+        pass
+    except requests.exceptions.HTTPError: 
+        return 'HTTPError was raised'
+    else:
+        if response.status_code == 200:
+            joke = response.json()['value']['joke']
+        else:
+            joke = 'No jokes'
+    return joke
+
+def next_level_get_joke():
+    url = 'http://api.icndb.com/jokes/random'
+
+    # print(type(requests.exceptions.Timeout))
+
+    try:
+        response = requests.get(url, timeout=30)
+        response.raise_for_status()
+
+    except requests.exceptions.Timeout:
+        return 'Timeout Error'
+    
+    except requests.exceptions.ConnectionError:
+        pass
+    except requests.exceptions.HTTPError as e:
+        status_code = e.response.status_code
+
+        if status_code in [503, 504]:
+            pass
+        else:
+            pass   
+        return 'HTTPError was raised'
+    else:
+        if response.status_code == 200:
+            joke = response.json()['value']['joke']
+        else:
+            joke = 'No jokes'
     return joke
 
 if __name__ == '__main__':
--- python_mocks/tests.py
+++ python_mocks/tests.py
@@ -1,10 +1,12 @@
 import unittest
 from unittest.mock import MagicMock, patch
 
+import responses
 import requests.exceptions
+from requests import HTTPError
 from requests.exceptions import Timeout
 
-from main import add, len_joke, get_joke, get_pro_joke
+from main import add, len_joke, get_joke, pro_get_joke, advance_get_joke, next_level_get_joke
 
 class TestAdd(unittest.TestCase):
     def test_add(self):
@@ -40,12 +42,32 @@ class TestJoke(unittest.TestCase):
     
     @patch('main.requests')
     def test_get_joke_timeout_exception(self, mock_requests):
-        #fix: for bellow mentioned mainblem
+        #fix: for bellow mentioned problem: assign mock exception a real exception module
         mock_requests.exceptions = requests.exceptions
         mock_requests.get.side_effect = Timeout('Seems that the server is down')
         # TypeError: catching classes thst do not inherit frim the BaseException is not allowed.
         # MagicMock class/obj is not subclass of BaseException class
-        self.assertEqual(get_pro_joke(), 'No jokes')
+        self.assertEqual(pro_get_joke(), 'Timeout Error')
+
+    @patch('main.requests')
+    def test_get_joke_raise_for_status(self, mock_requests):
+        mock_requests.exceptions = requests.exceptions
+        
+        mock_response = MagicMock(status_code=403)
+        mock_response.raise_for_status.side_effect = HTTPError('Something goes wrong')
+        mock_requests.get.return_value = mock_response
+        
+        self.assertEqual(advance_get_joke(), 'HTTPError was raised')
+        #self.assertRaises(HTTPError, advance_get_joke())
+
+    @responses.activate
+    def test_get_joke_returns_a_joke(self):
+        responses.get(
+            url='http://api.icndb.com/jokes/random',
+            json={'value': {'joke': 'hello world'}},
+            status=200
+        )
+        self.assertEqual(next_level_get_joke(), 'hello world')
 
 if __name__ == '__main__':
     unittest.main()
